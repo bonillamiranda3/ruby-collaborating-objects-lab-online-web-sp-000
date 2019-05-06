7f9e1ecd7bf6a6b53deb3f69c170e0d487bfa1e6
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-06 22:23:01 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,8 +10,13 @@ class MP3Importer
   end
 
   def import
-      self.files.each do |file|
-        Song.new_by_filename(file)
-      end
+    self.files.each do |file|
+      # parsed = file.split(" - ")
+      # artist = parsed[0]
+      # title = parsed[1]
+      # genre = parsed[2].split(".")[0]
+      # file_type = parsed[2].split(".")[1]
+      Song.new_by_filename(file)
+    end
   end
-end
+end 
