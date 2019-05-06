1ba4b33178c1f358d9fb6cc3ed0f275d74f9eb82
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-06 22:05:49 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist
+  atrr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_songs(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    if find(name) == nil 
+      then create(name)
+    else find(name)
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+  end
+end
+end
